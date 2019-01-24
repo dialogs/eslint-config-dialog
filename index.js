7232b4859906b172cebff6ae01ef7afdf4e7391e
meafmira <o.nechiporenko@dlg.im>
2019-01-24 17:06:59 +0300

    chore: babel rules

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ module.exports = {
       version: '16',
     },
   },
-  rules: Object.assign({}, eslint, react),
+  rules: Object.assign({}, eslint, react, {
+    'babel/new-cap': 1,
+    'babel/camelcase': 1,
+    'babel/no-invalid-this': 1,
+    'babel/object-curly-spacing': 1,
+    'babel/quotes': 1,
+    'babel/semi': 1,
+    'babel/no-unused-expressions': 1,
+    'babel/valid-typeof': 1,
+  }),
   globals: Object.assign({}, globals),
 };
