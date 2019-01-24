634b45dc7038c6ae29f42c2dd83fb225f6c7cd6d
meafmira <o.nechiporenko@dlg.im>
2019-01-24 17:08:20 +0300

    chore: removed some rules

--- index.js
+++ index.js
@@ -24,11 +24,9 @@ module.exports = {
     },
   },
   rules: Object.assign({}, eslint, react, {
-    'babel/new-cap': 1,
     'babel/camelcase': 1,
     'babel/no-invalid-this': 1,
     'babel/object-curly-spacing': 1,
-    'babel/quotes': 1,
     'babel/semi': 1,
     'babel/no-unused-expressions': 1,
     'babel/valid-typeof': 1,
