ceb6409eb3c795a7dbca08c79610726e2cc0b2cb
meafmira <o.nechiporenko@dlg.im>
2019-01-24 17:08:54 +0300

    chore: removed wrong rule

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ module.exports = {
   rules: Object.assign({}, eslint, react, {
     'babel/camelcase': 1,
     'babel/no-invalid-this': 1,
-    'babel/object-curly-spacing': 1,
     'babel/semi': 1,
     'babel/no-unused-expressions': 1,
     'babel/valid-typeof': 1,
