f86100cccc09aa4cf4fd5c3ca8f4a93a1dfae340
Elina Denisova <elin.rinnel@gmail.com>
2019-07-09 17:10:00 +0300

    refactor(eslint):  delete some rules that useless or already exist in prettier

--- src/rules/eslint/flowtype.js
+++ src/rules/eslint/flowtype.js
@@ -16,65 +16,11 @@ module.exports = {
     2,
     'always'
   ],
-  // Prevent usage 'mixed' type annotations.
-  'flowtype/no-mixed': 2,
-  // Requires that all function parameters have type annotations.
-  'flowtype/require-parameter-type': [
-    2,
-    {
-      // Exclude only concise arrow functions
-      'excludeArrowFunctions': 'expressionsOnly'
-    }
-  ],
   // Warns against weak type annotations any, Object and Function.
   'flowtype/no-weak-types': 2,
   // Enforces comma between properties in Flow object types.
   'flowtype/object-type-delimiter': [
     2,
     'comma'
-  ],
-  // Requires that functions have return type annotation.
-  'flowtype/require-parameter-type': [
-    2,
-    {
-      // Exclude only concise arrow functions
-      'excludeArrowFunctions': 'expressionsOnly'
-    }
-  ],
-  // Requires that functions have return type annotation.
-  'flowtype/require-return-type': [
-    2,
-    'always',
-    {
-      'excludeMatching': [
-        'render*',
-      ],
-      'annotateUndefined': 'never'
-    }
-  ],
-  // Enforces consistent use of semicolons after type aliases.
-  'flowtype/semi': [
-    2,
-    'always'
-  ],
-  // Enforces consistent spacing after the type annotation colon.
-  'flowtype/space-after-type-colon': [
-    2,
-    'always'
-  ],
-  // Enforces consistent spacing before the opening < of generic type annotation parameters.
-  'flowtype/space-before-generic-bracket': [
-    2,
-    'never'
-  ],
-  // Enforces consistent spacing before the type annotation colon.
-  'flowtype/space-before-type-colon': [
-    2,
-    'never'
-  ],
-  // Enforces consistent spacing around union and intersection type separators (| and &).
-  'flowtype/union-intersection-spacing': [
-    2,
-    'always'
   ]
 }
